3c9ed732d3442d7eaa6e71a9ba496618c3cdd59f
BrijeshMakwana18 <bamakwana2000@gmail.com>
2022-05-23 21:06:28 +0530

    svg icons added

---
+++ components/icons/hamburger.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HamBurger = () => (
+  <svg viewBox="0 0 100 80" width="30" height="30">
+    <rect width="70" height="15" rx="10"></rect>
+    <rect y="30" width="100" height="15" rx="10"></rect>
+    <rect y="60" width="70" height="15" rx="10"></rect>
+  </svg>
+);
+
+export default HamBurger;
